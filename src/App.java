4f684deb9405b2e2ad7cdac3f16992e37d12e475
SvenTigges <sven.tigges@web.de>
2021-05-31 14:32:36 +0200

    Verdopplung Objekt

--- src/App.java
+++ src/App.java
@@ -2,9 +2,15 @@ public class App {
     
     public static void main(String[] args) {
         
-        Cat cat = new Cat();
-        System.out.println("Blick von aussen: " + cat);
-        cat.tellYourAddress();
+        Cat cat1 = new Cat();
+        System.out.println("Blick von aussen: " + cat1);
+        cat1.tellYourAddress();
+
+        System.out.println("---------------------------");
         
+        Cat cat2 = new Cat();
+        System.out.println("Blick von aussen: " + cat2);
+        cat2.tellYourAddress();
+
     }
 }
