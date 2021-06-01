893f6d18406fca48fed04dbb2bdfe6dfa22a6477
SvenTigges <sven.tigges@web.de>
2021-06-01 09:21:57 +0200

    Ref_03_Felder | Wertzuweisung von außen

--- src/App.java
+++ src/App.java
@@ -4,13 +4,15 @@ public class App {
         
         Cat cat1 = new Cat();
         System.out.println("Blick von aussen: " + cat1);
+        cat1.name = "Grizabella"; 
         cat1.tellYourAddress();
-
+        cat1.tellYourName();
         System.out.println("---------------------------");
         
         Cat cat2 = new Cat();
         System.out.println("Blick von aussen: " + cat2);
+        cat2.name = "Alonzo"; 
         cat2.tellYourAddress();
-
+        cat2.tellYourName();
     }
 }
