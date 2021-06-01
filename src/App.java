2a7ab15793e5c920110c3ecf6d4ce20d7f0e50f1
SvenTigges <sven.tigges@web.de>
2021-06-01 14:55:46 +0200

    add_return + ausgabe

--- src/App.java
+++ src/App.java
@@ -1,17 +1,27 @@
-public class App {
+public class App 
+{
     
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         
         Cat cat1 = new Cat("Grizabella", "white", 29);
-        cat1.tellYourAtrributes("name");
-        cat1.tellYourAtrributes("furColor");
-        cat1.tellYourAtrributes("age");
+        ausgabe(cat1.tellYourAtrributes("name"));
+        ausgabe(cat1.tellYourAtrributes("furColor"));
+        ausgabe(cat1.tellYourAtrributes("age"));
+        ausgabe(cat1.tellYourAtrributes("ashdhaahd"));
 
         System.out.println("---------------------------");
         
         Cat cat2 = new Cat("Alonzo", "black", 35);
-        cat2.tellYourAtrributes("name");
-        cat2.tellYourAtrributes("furColor");
-        cat2.tellYourAtrributes("age");
+        ausgabe(cat2.tellYourAtrributes("name"));
+        ausgabe(cat2.tellYourAtrributes("furColor"));
+        ausgabe(cat2.tellYourAtrributes("age"));
+        ausgabe(cat2.tellYourAtrributes("ashdhaahd"));
     }
+
+
+public static void ausgabe(String outputStr) 
+{
+    System.out.println(outputStr);
+}
 }
