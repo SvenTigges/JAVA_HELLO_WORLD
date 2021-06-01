fda473b8d55326991da07a25e4fa73b0a6202e12
SvenTigges <sven.tigges@web.de>
2021-06-01 10:45:44 +0200

    add_constructor

--- src/App.java
+++ src/App.java
@@ -4,14 +4,19 @@ public class App {
         
         Cat cat1 = new Cat();
         System.out.println("Blick von aussen: " + cat1);
-        cat1.name = "Grizabella"; 
+        cat1.changeYourName("Grizabella");
+        // changeYourAge
+        //changeYourFurColor
+
         cat1.tellYourAddress();
         cat1.tellYourName();
+        
+        
         System.out.println("---------------------------");
         
         Cat cat2 = new Cat();
         System.out.println("Blick von aussen: " + cat2);
-        cat2.name = "Alonzo"; 
+        cat2.changeYourName("Alonzo");
         cat2.tellYourAddress();
         cat2.tellYourName();
     }
