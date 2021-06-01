1257e2475cf74c84f7472386b8df6a818ed1465d
SvenTigges <sven.tigges@web.de>
2021-06-01 11:03:49 +0200

    05_custom_constructor

--- src/Cat.java
+++ src/Cat.java
@@ -5,22 +5,16 @@ public class Cat
     public String furColor;
     public int age; 
 
-    public Cat(String name,String furColor,int age){
-        this.name = name; 
+    public Cat(String name, String furColor, int age) {
+        this.name = name;
         this.furColor = furColor;
         this.age = age;
-    }
-
-
-    public void tellYourAddress(){
-        System.out.println("Blick von innen: " + this);
 
     }
+
     public void tellYourName() {
         System.out.println(this.name);
     }
-    public void changeYourName(String name) {
-        this.name = name;
-    }
+    
     
 }
