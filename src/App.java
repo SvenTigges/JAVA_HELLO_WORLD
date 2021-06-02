0cf8e8944e0c3acee6c5378d1a91fc39eaeef552
SvenTigges <sven.tigges@web.de>
2021-06-02 08:57:23 +0200

    Cat: isFemale

--- src/App.java
+++ src/App.java
@@ -4,24 +4,24 @@ public class App
     public static void main(String[] args) 
     {
         
-        Cat cat1 = new Cat("Grizabella", "white", 29);
+        Cat cat1 = new Cat("Grizabella", "white", 29, true);
         ausgabe(cat1.tellYourAtrributes("name"));
         ausgabe(cat1.tellYourAtrributes("furColor"));
         ausgabe(cat1.tellYourAtrributes("age"));
-        ausgabe(cat1.tellYourAtrributes("ashdhaahd"));
+        
 
         System.out.println("---------------------------");
         
-        Cat cat2 = new Cat("Alonzo", "black", 35);
+        Cat cat2 = new Cat("Alonzo", "black", 35, false);
         ausgabe(cat2.tellYourAtrributes("name"));
         ausgabe(cat2.tellYourAtrributes("furColor"));
         ausgabe(cat2.tellYourAtrributes("age"));
-        ausgabe(cat2.tellYourAtrributes("ashdhaahd"));
+        
     }
 
 
-public static void ausgabe(String outputStr) 
-{
-    System.out.println(outputStr);
-}
+    public static void ausgabe(String outputStr) 
+    {
+        System.out.println(outputStr);
+    }
 }
--- src/Cat.java
+++ src/Cat.java
@@ -4,13 +4,17 @@ public class Cat
     public String name; 
     public String furColor;
     public int age; 
+    public boolean isFemale; 
 
-    public Cat(String name, String furColor, int age) {
+
+    public Cat(String name, String furColor, int age, boolean isFemale) {
         this.name = name;
         this.furColor = furColor;
         this.age = age;
+        this.isFemale = isFemale;
     }
-   
+
+
     public String tellYourAtrributes(String chechStr) {
         switch (chechStr) {
             case "name":
@@ -18,14 +22,43 @@ public class Cat
             case "furColor":
                 return this.furColor;  
             case "age":
-                return String.valueOf(this.age);
+                return this.checkCompliance(); 
+                //return Integer.toString(this.age);
+                //return String.valueOf(this.age);
             default:
                 return "error";
         }
         
     }
+    public String checkCompliance() {
+        if (isFemale) 
+        {
+            return "This is an inappropriate qestion!";
+        } 
+        else 
+        {
+            return Integer.toString(this.age);  
+        }
+    }
 }  
-/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*  Erste überlegung 
     public void tellYourName() {
         System.out.println(this.name);
     } 
